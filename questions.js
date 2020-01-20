9d3589c309ce250ea971238e93a0be126f520f31
John Davis <johnedavis624@gmail.com>
2020-01-19 21:59:53 -0600

    added for loop with if and else

--- questions.js
+++ questions.js
@@ -25,3 +25,17 @@ var questions = [
       answer: "quotes"
     },
   ];
+
+for(i = 0; i < questions.length; i++){
+  
+  // var response = need to make a variable for students selected answer and store it in local memory
+
+  if(response === question[i].answer){
+      // in here I put what happens if questions answered correctly
+  }
+
+  else(response !== questions[i].answer){
+    // in here put what happens if question is answered incorrectly
+  }
+
+}
