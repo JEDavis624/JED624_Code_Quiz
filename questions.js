6485604e4ff44b0d21d19f9963bf8b94574cdf4d
John Davis <johnedavis624@gmail.com>
2020-01-20 18:09:40 -0600

    added quiz-script.js

--- questions.js
+++ questions.js
@@ -26,16 +26,4 @@ var questions = [
     },
   ];
 
-for(i = 0; i < questions.length; i++){
-  
-  // var response = need to make a variable for students selected answer and store it in local memory
 
-  if(response === question[i].answer){
-      // in here I put what happens if questions answered correctly
-  }
-  
-  else(response !== questions[i].answer){
-    // in here put what happens if question is answered incorrectly
-  }
-
-}
---
+++ quiz-script.js
@@ -0,0 +1,13 @@
+for(i = 0; i < questions.length; i++){
+  
+    // var response = need to make a variable for students selected answer and store it in local memory
+  
+    if(response === question[i].answer){
+        // in here I put what happens if questions answered correctly
+    }
+    
+    else(response !== questions[i].answer){
+      // in here put what happens if question is answered incorrectly
+    }
+  
+  }
